79ba741af04603574bec35dcdbb55763c9a62bee
Wilsonlim3 <31687911+Wilsonlim3@users.noreply.github.com>
2018-10-09 11:36:32 +0700

    CalendarTemplate

--- Training/app/src/main/java/com/example/wijen/training/Calendar.java
+++ Training/app/src/main/java/com/example/wijen/training/Calendar.java
@@ -10,13 +10,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.github.sundeepk.compactcalendarview.*;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Calendar extends AppCompatActivity {
 
@@ -24,6 +28,7 @@ public class Calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        final SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
 
 //        CalendarView simpleCalendarView = findViewById(R.id.simpleCalendarView);
 //        simpleCalendarView.setFirstDayOfWeek(2);
@@ -45,17 +50,19 @@ public class Calendar extends AppCompatActivity {
 //
 //            }
 //        });
+        final TextView monthTxt = findViewById(R.id.month);
+        final TextView eventTxt = findViewById(R.id.eventTxt);
         final CompactCalendarView compactCalendarView = findViewById(R.id.compactcalendar_view);
+        monthTxt.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
         // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
         // Use constants provided by Java Calendar class
        // compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
 
-        // Add event 1 on Sun, 07 Jun 2015 18:20:51 GMT
-        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
+        // Add event 1 on Sun, 12 Okt 2018 12:50:00 GMT
+        Event ev1 = new Event(Color.GREEN, 1539323400000L, "Some extra data that I want to store.");
         compactCalendarView.addEvent(ev1);
 
-        // Added event 2 GMT: Sun, 07 Jun 2015 19:10:51 GMT
-        Event ev2 = new Event(Color.GREEN, 1433704251000L);
+        Event ev2 = new Event(Color.GREEN, 1539450000000L, "Add event manual");
         compactCalendarView.addEvent(ev2);
 
         // Query for events on Sun, 07 Jun 2015 GMT.
@@ -70,13 +77,20 @@ public class Calendar extends AppCompatActivity {
         compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
             public void onDayClick(Date dateClicked) {
+                monthTxt.setText(dateFormatForMonth.format(dateClicked));
                 List<Event> events = compactCalendarView.getEvents(dateClicked);
+
+                eventTxt.setText(events.toString());
+                for(Event event : events){
+                    Log.i("Events","="+event.getData());
+                }
                 Log.d("asd", "Day was clicked: " + dateClicked + " with events " + events);
             }
 
             @Override
             public void onMonthScroll(Date firstDayOfNewMonth) {
                 Log.d("asd", "Month was scrolled to: " + firstDayOfNewMonth);
+                monthTxt.setText(dateFormatForMonth.format(firstDayOfNewMonth));
             }
         });
 
